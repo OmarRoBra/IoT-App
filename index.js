4a92287c5f0848a5989d812e69fddc5f1b9935f8
Omar Rodriguez <jrodriguez40@gmail.com>
2020-06-14 17:14:52 -0500

    xd

---
+++ index.js
@@ -0,0 +1,128 @@
+let date = new Date();
+let time = date.getTime();
+let counter2 = time;
+
+//Guardar nave
+    function agregarNave(){
+    let nombre = document.getElementById("nombreNave").value;
+    let descripcion = document.getElementById("descripcion").value;
+
+    counter2 += 1;
+    console.log(counter2);
+    let naves = {
+        id: counter2,
+        nombre,
+        descripcion
+    };
+    console.log(naves);
+    let db = firebase.database().ref("naves/"+nombre+"/informacion");
+    db.set(naves)
+        .then(() => {
+            console.log("Nave añadida");
+            document.getElementById("nombreNave").value = "";
+            document.getElementById("descripcion").value = "";
+        })
+        .catch((err) => {
+            console.error("Error añadiendo nave: ", err);
+        });
+}
+
+
+//Mostrar naves
+function mostrarNaves(){
+    let mostrarNaves = document.getElementById('navesDescripcion');
+    let naves= firebase.database().ref("naves/");
+    naves.on('value',function(snapshot){
+        mostrarNaves.innerHTML = '';
+        snapshot.forEach(element => {
+            mostrarNaves.innerHTML += `
+            <a href="#collapseCardExample" class="d-block card-header py-3" data-toggle="collapse"
+                role="button" aria-expanded="true" aria-controls="collapseCardExample">
+                <h6 class="m-0 font-weight-bold text-primary">${element.val().informacion.nombre}</h6>
+            </a>
+            <div class="collapse show" id="collapseCardExample">
+                <div class="card-body">
+                    ${element.val().informacion.descripcion}
+                    <div class="my-2"></div>
+                    <button class="btn btn-primary" onclick="redireccionar('${element.val().informacion.nombre}')">Más información <i class="fas fa-arrow-right"></i></button>
+                    <div class="my-2"></div>
+                    <button type="submit" style="color:white; display: inline-block;" data-toggle="modal" data-target="#exampleModal" class="btn btn-info"
+                    onclick="actualizarNave(${element.val().informacion.id},'${element.val().informacion.nombre}','${element.val().informacion.descripcion}')"
+                     >Editar</button>
+                    <button class="btn btn-danger" style="color:white; display: inline-block;" type="submit" 
+                    onclick="eliminarNave('${element.val().informacion.nombre}')">Eliminar</button>
+                </div>
+            </div>
+            `
+        });
+    });
+
+    naves.on("child_removed", function(data){
+        let navesValue=data.val();
+        let naves= document.getElementById(navesValue.nombre);
+        if(naves){naves.remove()}
+        console.log("Se elimino ", navesValue);
+    });
+}
+
+
+//Actualizar sensor
+function actualizarNave(id,nombre,descripcion){   
+    console.log(id,nombre,descripcion);
+    document.getElementById("nombreNave").value=nombre;
+    document.getElementById("descripcion").value=descripcion;
+
+    let actualizar = document.getElementById("actualizarNave");
+    actualizar.style.display = 'inline-block';
+    let cancelar = document.getElementById("cancelarActualizar");
+    cancelar.style.display = 'inline-block';
+    let botonGuardar = document.getElementById('agregarNave');
+    botonGuardar.style.display = 'none';
+    let modal = document.getElementById('cancelarModal');
+    modal.style.display = 'none';
+  
+    actualizar.onclick=function(){
+      let nombreNave = document.getElementById("nombreNave").value;
+      let descripcion = document.getElementById("descripcion").value;
+      let data = {
+        id,
+        nombreNave,
+        descripcion
+      };
+      firebase.database().ref('naves/' +nombreNave+"/informacion/").update(data).then(()=>{
+      console.log("Nave actualizada");
+      actualizar.innerHTML="Agregar"
+      document.getElementById("nombreNave").value='';
+      document.getElementById("descripcion").value='';
+    }).catch(err =>{
+      console.log('ALGO FALLÓ, CORRE: ',err);
+    });
+  }
+  }
+
+  function cancelarEditar(){
+    let botonGuardar = document.getElementById('agregarNave');
+    botonGuardar.style.display = 'inline-block';
+    botonGuardar.innerHTML="Agregar";
+    let modal = document.getElementById('cancelarModal');
+    modal.style.display = 'inline-block';
+    let actualizar = document.getElementById("actualizarNave");
+    actualizar.style.display = 'none';
+    let cancelar = document.getElementById("cancelarActualizar");
+    cancelar.style.display = 'none';
+    document.getElementById("nombreNave").value='';
+    document.getElementById("descripcion").value='';
+  }
+
+function redireccionar(nombre){
+    location.href='sensores.html'  
+    localStorage.setItem('nave', nombre)  
+}
+function eliminarNave(nombre){
+    let nave= firebase.database().ref("naves/"+nombre);
+    nave.remove().then(()=>{
+      console.log('Nave eliminado');
+    }).catch(err=>{
+     console.log('Error al eliminar la nave:',err);
+    });
+}
---
+++ main.js
@@ -0,0 +1,197 @@
+/* let firebaseConfig = {
+    apiKey: "AIzaSyDlBW8k0czNHhdDwK265IqAtt_qgNKb7SM",
+    authDomain: "control-de-un-invernadero.firebaseapp.com",
+    databaseURL: "https://control-de-un-invernadero.firebaseio.com",
+    projectId: "control-de-un-invernadero",
+    storageBucket: "control-de-un-invernadero.appspot.com",
+    messagingSenderId: "103114132808",
+    appId: "1:103114132808:web:806b125d430ea4869ef77d",
+    measurementId: "G-2G03EB94PP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig); */
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBEs_fOhXJS8inmyxcGbss3wESHmWypxoI",
+  authDomain: "redes-7f174.firebaseapp.com",
+  databaseURL: "https://redes-7f174.firebaseio.com",
+  projectId: "redes-7f174",
+  storageBucket: "redes-7f174.appspot.com",
+  messagingSenderId: "444028512040",
+  appId: "1:444028512040:web:c151f914e67bc889ac521d",
+  measurementId: "G-TK8QYSLE1Z"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+  let d = new Date();
+  let t = d.getTime();
+  let counter = t;
+
+//Guardar sensores
+function guardarSensor(){
+      let nombreNave=localStorage.getItem('nave');
+      let nombre = document.getElementById("nombre").value;
+      let tipo;
+      let minimo = document.getElementById("minimo").value;
+      let maximo = document.getElementById("maximo").value;
+      switch(nombre){
+        case 'altitud':
+          tipo = 'BMP180'
+          break;
+        case 'distancia':
+          tipo = 'HC-SR04'
+          break;
+        case 'humedad':
+          tipo = 'DHT11'
+          break;
+        case 'luz':
+          tipo = 'LDR'
+          break;
+        case 'servomotor':
+          tipo = 'Servomotor SG90'
+          break;
+        case 'presion':
+          tipo = 'BMP180'
+          break;
+        case 'temperatura':
+          tipo = 'BMP180'
+          break;
+      }
+      counter+=1;
+      console.log(counter);
+      let sensor={
+          id:counter,
+          nombre,
+          tipo,
+          minimo,
+          maximo
+      }
+      let db=firebase.database().ref("naves/"+nombreNave+"/sensores/"+nombre);
+      db.set(sensor).then(()=>{
+        console.log("Sensor añadido");
+        document.getElementById("nombre").value='';
+        document.getElementById("tipo").value='';
+        document.getElementById("minimo").value='';
+        document.getElementById("maximo").value='';
+      }).catch(err =>{
+        console.error("Error añadiendo sensor: ", err);
+      });
+  }
+
+//Leer datos - mostrar tabla
+function leerSensor(){
+  let nombreNave=localStorage.getItem('nave');
+  let sensor= firebase.database().ref("naves/"+nombreNave+"/sensores/");
+  sensor.on("child_added", function(data){
+      let sensorValue=data.val();
+      console.log(sensorValue)
+      document.getElementById("tabla").innerHTML+=`
+      <tr id="${sensorValue.nombre}">
+      <th scope="row">${sensorValue.id}</th>
+      <td>${sensorValue.nombre}</td>
+      <td>${sensorValue.tipo}</td>
+      <td>${sensorValue.minimo}</td>
+      <td>${sensorValue.maximo}</td>
+      <td><button type="submit" style="color:white" class="btn btn-info"
+      onclick="actualizarSensor(${sensorValue.id},'${sensorValue.nombre}','${sensorValue.minimo}','${sensorValue.maximo}')"
+      >Editar</button></td>
+      <td><button class="btn btn-danger" type="submit" onclick="eliminarSensor('${sensorValue.nombre}')">Eliminar</button></td>
+      `
+  });
+
+  sensor.on("child_removed", function(data){
+    let sensorValue=data.val();
+    let sensores= document.getElementById(sensorValue.nombre);
+    if(sensores){sensores.remove()}
+  // console.log("Se elimino ", sensorValue);
+});
+
+sensor.on("child_changed",function(data){
+    let sensorValue=data.val();
+    let sensorFila= document.getElementById(sensorValue.nombre);
+   console.log("Se edito ", sensorValue);
+   sensorFila.children[1].innerText=sensorValue.nombre;
+   sensorFila.children[2].innerText=sensorValue.tipo;
+   sensorFila.children[3].innerText=sensorValue.minimo;
+   sensorFila.children[4].innerText=sensorValue.maximo;
+});
+}
+
+//Actualizar sensor
+function actualizarSensor(id,nombre,minimo,maximo){   
+  let nombreNave=localStorage.getItem('nave');
+  document.getElementById("nombre").value=nombre;
+  document.getElementById("minimo").value=minimo;
+  document.getElementById("maximo").value=maximo;
+  let actualizar = document.getElementById("button2");
+  actualizar.style.display = 'inline-block';
+  let cancelar = document.getElementById("button3");
+  cancelar.style.display = 'inline-block';
+  let botonGuardar = document.getElementById('button1');
+  botonGuardar.style.display = 'none';
+
+  actualizar.onclick=function(){
+    let nombre = document.getElementById("nombre").value;
+    let tipo;
+    let minimo = document.getElementById("minimo").value;
+    let maximo = document.getElementById("maximo").value;
+    switch(nombre){
+      case 'altitud','presion','temperatura':
+        tipo = 'BMP180'
+        break;
+      case 'distancia':
+        tipo = 'HC-SR04'
+        break;
+      case 'humedad':
+        tipo = 'DHT11'
+        break;
+      case 'luz':
+        tipo = 'LDR'
+        break;
+      case 'servomotor':
+        tipo = 'Servomotor SG90'
+        break;
+    }
+    let data = {
+      id,
+      nombre,
+      tipo,
+      minimo,
+      maximo,
+    };
+    firebase.database().ref("naves/"+nombreNave+"/sensores/" +nombre).update(data).then(()=>{
+    console.log("Sensor actualizado");
+    actualizar.style.display = 'none';
+    cancelar.style.display = 'none';
+    botonGuardar.style.display = 'inline-block';
+    document.getElementById("nombre").value='';
+    document.getElementById("minimo").value='';
+    document.getElementById("maximo").value='';
+  }).catch(err =>{
+    console.log('ALGO FALLÓ, CORRE: ',err);
+  });
+}
+}
+
+//Eliminar sensor
+function eliminarSensor(nombre){
+  let nombreNave=localStorage.getItem('nave');
+   let sensor= firebase.database().ref("naves/"+nombreNave+"/sensores/"+nombre);
+   sensor.remove().then(()=>{
+     console.log('Sensor eliminado');
+   }).catch(err=>{
+    console.log('Error al eliminar el sensor:',err);
+   });
+}
+
+//MODAL
+$("#exampleModal").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget); // Button that triggered the modal
+  var recipient = button.data(""); // Extract info from data-* attributes
+  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+  var modal = $(this);
+  modal.find(".modal-title").text("New message to " + recipient);
+  modal.find(".modal-body input").val(recipient);
+});
